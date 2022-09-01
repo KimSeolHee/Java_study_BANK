20016c4ec65eef0f25c38c2df56841bb89f5490c
KimSeolHee <tjfgml64@nate.com>
2022-09-01 17:47:14 +0900

    add

--- src/main/webapp/resources/js/board.js
+++ src/main/webapp/resources/js/board.js
@@ -3,7 +3,7 @@ const fileAdd = document.getElementById('fileAdd');
 const div3 = document.getElementById('div3');
 
 let count = 0;
-
+let idx = 0;
 fileAdd.addEventListener("click", function(){
     count = count +1;
     if(count > 5){
@@ -15,7 +15,9 @@ fileAdd.addEventListener("click", function(){
     let c= document.createAttribute('class');
     c.value = "form-group pt-3";
     div.setAttributeNode(c);
-    addFiles.append(div); 
+    c = document.createAttribute('id');
+    c.value = 'file'+idx;
+    div.setAttributeNode(c);
             
     //자식 Element Label생성
     let label = document.createElement('label');
@@ -48,9 +50,38 @@ fileAdd.addEventListener("click", function(){
         
     //div안에 input태그 넣기
     div.appendChild(input);
+
+
+    //delete버튼
+    let del = document.createElement('button');
+    let type2 = document.createAttribute('type');
+    type2.value = 'button';
+    del.setAttributeNode(type2);
+    let style = document.createAttribute('style');
+    style.value = 'border: none;color:red;';
+    del.setAttributeNode(style);
+    let delClass = document.createAttribute('class');
+    delClass.value = 'del';
+    del.setAttributeNode(delClass);
+    let text = document.createTextNode("삭제");
+    del.appendChild(text);
+    div.appendChild(del);
+    buttonAttr = document.createAttribute('title');
+    buttonAttr.value=idx;
+    del.setAttributeNode(buttonAttr);
+
         
     //addFiles에 div를 append함
     addFiles.append(div);
-    });     
+    idx++;
+    });
 
+addFiles.addEventListener("click", function(event){
+    if(event.target.className == 'del'){
+        for(let i = 0;i < idx; i++){
+            let file = document.getElementById('file'+i);
+            file.remove();
+        }
+    }
+})
 
